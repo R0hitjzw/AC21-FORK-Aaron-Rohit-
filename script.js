952bcf7c7a96b40a9e9639bccf3e7ea1773014b4
R0hitjzw <rohit.jaswal@estudiant.fjaverianas.com>
2025-12-16 19:31:05 +0100

    COMMIT INITIAL HTML - JS

---
+++ script.js
@@ -0,0 +1,4 @@
+const form = document.getElementById('todo-form');
+const input = document.getElementById('task-input');
+const taskList = document.getElementById('task-list');
+const clearAllBtn = document.getElementById('clear-all');
